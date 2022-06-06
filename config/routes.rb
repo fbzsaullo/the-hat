a245342ed37a8203f0327d3060b42d27be21f792
Fabrizio Saullo <fabriziodsaullo@gmail.com>
2022-06-06 17:02:36 -0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'languages/show'
-  get 'houses/index'
-  get 'questions/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
