b857c9377003c32c2bb2e6dd4ba139ff712ab883
Fabrizio Saullo <fabriziodsaullo@gmail.com>
2022-06-06 16:48:10 -0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :survays, only: [:index] do
+    resources :questions, only: [:show]
+  end
+
+  resources :houses, only: [:index] do
+    resources :languages, only: [:show]
+  end
 end
