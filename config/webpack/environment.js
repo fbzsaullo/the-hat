485bfb465851bed8f26e32657239488a13e3197c
Fabrizio Saullo <fabriziodsaullo@gmail.com>
2022-06-06 10:55:16 -0300

    Initial commit with minimal template from https://github.com/lewagon/rails-templates

---
+++ config/webpack/environment.js
@@ -0,0 +1,5 @@
+const { environment } = require('@rails/webpacker')
+
+// Preventing Babel from transpiling NodeModules packages
+environment.loaders.delete('nodeModules');
+module.exports = environment
