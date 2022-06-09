0de47990bcf5324f19069024b7dfe7ec0f38c338
Erick Valente <erickhwk@gmail.com>
2022-06-09 18:14:41 -0300

    assign house method created

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,19 @@
 class QuestionsController < ApplicationController
 
+  # def result
+  #   result = current_user.answers.joins(:question).group("questions.house_id").sum("answers.score").sort_by(&:last).reverse.first
+  #   house = House.find(result.first)
+  # end
+
   def show
     @answered_questions = current_user.answers.pluck(:question_id)
     @question = Question.where.not(id: @answered_questions).sample
     @answer = Answer.new
+
     if @question.nil?
+      result = current_user.answers.joins(:question).group("questions.house_id").sum("answers.score").sort_by(&:last).reverse.first
+      house = House.find(result.first)
+      current_user.house_id = house.id
       redirect_to root_path
     end
   end
