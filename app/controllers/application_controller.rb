7e79340efb0e58772fe3c39d0ad081a062fa9d2a
Fabrizio Saullo <fabriziodsaullo@gmail.com>
2022-06-10 14:58:36 -0300

    authenticate_user!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   # [...]
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :authenticate_user!
+  
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo])
