80497758f33e29d28980e1686920e0de4a44c327
Fabrizio Saullo <fabriziodsaullo@gmail.com>
2022-06-09 22:40:22 -0300

    add /profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/profile', to: 'pages#profile'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :questions, only: [:show]
   resources :answers, only: [:create]
