f7dbc5752b48563b393afbddec95ad32ba9d3aa5
Fabrizio Saullo <fabriziodsaullo@gmail.com>
2022-06-10 12:24:13 -0300

    new house routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/profile', to: 'pages#profile'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :questions, only: [:show]
   resources :answers, only: [:create]
 
-  resources :houses, only: [:index] do
-    resources :languages, only: [:show]
+  get "/houses/front-end", to: "houses#front-end"
+  get "/houses/back-end", to: "houses#back-end"
+  get "/houses/game-dev", to: "houses#gamedev"
+  get "/houses/data-science", to: "houses#data-science"
+
+  resources :houses, only: [:show], param: :name do
+    resources :languages, only: [:show], param: :name
   end
 end
