d1804836b7d6a943097edeb2ae59e74a5ab5d553
Erick Valente <erickhwk@gmail.com>
2022-06-13 18:00:53 -0300

    template done

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,7 +1,5 @@
 class HousesController < ApplicationController
-  def index
-    @houses = House.all
-  end
+  before_action :house_sort
 
   def frontend
     @house = House.find(1)
@@ -17,5 +15,10 @@ class HousesController < ApplicationController
 
   def datascience
     @house = House.find(4)
+  end
+
+  def house_sort
+    first_result = current_user.answers.joins(:question).group("questions.house_id").sum("answers.score").sort_by(&:last).reverse.first
+    @result = (first_result.last * 100) / 20
   end
 end
