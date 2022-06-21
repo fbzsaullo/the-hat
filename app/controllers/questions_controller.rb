178e0d5a1e86ee9d13859906bf32907af814d6e9
FabrÃizio Suallo <fabriziodsaullo@gmail.com>
2022-06-21 05:10:21 -0300

    questions redirect to profile page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -16,16 +16,9 @@ class QuestionsController < ApplicationController
       house = House.find(result.first)
       current_user.house_id = house.id
       current_user.save
-      case current_user.house_id
-      when 1
-        redirect_to houses_frontend_path
-      when 2
-        redirect_to houses_backend_path
-      when 3
-        redirect_to houses_gamedev_path
-      when 4
-        redirect_to houses_datascience_path
-      end
+
+      redirect_to profile_path
+
     end
   end
 
