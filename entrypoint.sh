0a88fda107a8fd3f9b17a1f399cd1046bc6935a7
Fabrizio Saullo <fabrizio.saullo-ext@dexian.com>
2026-06-11 23:27:33 -0300

    a

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,13 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
+# Write RAILS_MASTER_KEY to config/master.key if environment variable is present
+if [ -n "$RAILS_MASTER_KEY" ]; then
+  echo "RAILS_MASTER_KEY detected. Creating config/master.key..."
+  mkdir -p /app/config
+  echo "$RAILS_MASTER_KEY" > /app/config/master.key
+fi
+
 # Run migrations automatically in production
 if [ "$RAILS_ENV" = "production" ]; then
   echo "Running database migrations..."
