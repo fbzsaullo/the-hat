1aefb6e8e8adcb05c961b85123ba3c10061eae4b
Fabrizio Saullo <fabrizio.saullo-ext@dexian.com>
2026-06-11 23:15:25 -0300

    Fix render

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,11 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
+# Run migrations automatically in production
+if [ "$RAILS_ENV" = "production" ]; then
+  echo "Running database migrations..."
+  bundle exec rails db:migrate
+fi
+
 # Then exec the container's main process.
 exec "$@"
